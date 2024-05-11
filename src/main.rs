acc3a69694f471b63f7d436137c27159b8c43bbd
fedaykin <fremanfedaykin@gmail.com>
2024-05-11 15:28:09 +0900

    feat: rust scope ownership 테스트

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,27 @@
 
 fn main() {
+    let a: i64 = 10;
+    
+    println!("a = {}", a);
+    
+    let b: i64 = a + 10;
+    
+    println!("sum = {}", sum(a, b));
+    println!("a = {}", a);
+    println!("b = {}", b);
+    
     println!("Hello, world!");
+    
+    let s1 = String::from("hello");
+    // let s1 = "hello";
+    let s2 = s1;
+    println!("{}, world!", s1);
 }
+
+fn sum(a: i64, b: i64) -> i64 {
+    return a + b;
+}
+
+/*fn sum(a: i64, b: i64) -> i64 {
+    a + b
+}*/
